5dbe637665708367dfc75910904e3e97bebb1afd
Theodike <44587268+Theodikes@users.noreply.github.com>
2022-10-24 01:12:01 +0300

    Изменил импорты, чтобы было очевидно, что это импорт из текущей директории, а не какой-то сторонний пакет patool_unpack

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from os import listdir, remove, rmdir
 from os.path import isdir, isfile, splitext, basename, join, dirname, exists
-from patool_unpack import get_archive_format, check_archive_format, test_archive, extract_archive, ArchiveFormats
-from patool_unpack.util import PatoolError
+from .patool_unpack import get_archive_format, check_archive_format, test_archive, extract_archive, ArchiveFormats
+from .patool_unpack.util import PatoolError
 from typing import Optional, Tuple
 
 
